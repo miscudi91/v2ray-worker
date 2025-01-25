f57e664116d8522b6f79bc85bd1c1b83c308f829
miscud91 <miscudi91@gmail.com>
2025-01-25 23:45:42 +0500

    Update worker-configuration.d.ts

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,6 +1,6 @@
 interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	MY_KV_NAMESPACE:6152ce5591c444f49d750a7c12a587dd KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
